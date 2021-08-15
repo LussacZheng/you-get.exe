ebafb30107953b34cc9de3e040cfe752c2b6c010
Lussac Zheng <lussaczheng@gmail.com>
2021-08-15 14:12:05 +0800

    chore(devscripts): add `archive.bat`
    It is used to package all the files of the project, excluding
    those that are unnecessary when running `build.bat` .
    The main purpose is to provide a consistent package of source code,
    for building in the virtual machines.

---
+++ devscripts/archive.bat
@@ -0,0 +1,27 @@
+@rem - Encoding:utf-8; Mode:Batch; Language:en; LineEndings:CRLF -
+@echo off
+
+:: Set the root directory
+pushd "%~dp0"
+
+set "_output=you-get.exe.zip"
+
+if exist %_output% del /P .\%_output%
+
+pushd ..
+
+zip -r %_output% ^
+    bin/bzip2.dll ^
+    bin/dos2unix.exe ^
+    bin/zip.exe ^
+    build/file_version_info.txt ^
+    build/you-get.ico ^
+    repository/ ^
+    build.bat ^
+    poetry.lock poetry.toml pyproject.toml ^
+    README.md README_cn.md ^
+    -x "repository/you-get/.git/*"
+
+move %_output% devscripts\
+
+popd & popd
